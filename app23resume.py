a2ceb70e4f8a855cb151cca564173a55e2416e27
Rajatathilkar <125897588+Rajatathilkar@users.noreply.github.com>
2023-07-19 19:55:34 +0530

    Update app23resume.py

--- app23resume.py
+++ app23resume.py
@@ -35,16 +35,17 @@ def main():
         cleaned_resume=tfidf.transform([cleaned_resume])
         prediction_id=clf.predict(cleaned_resume)[0]
         st.write(prediction_id)
-category_mapping={'Data Science':6, 'HR':12, 'Advocate':0, 'Arts':1, 'Web Designing':24,
-     'Mechanical Engineer':16, 'Sales':22, 'Health and fitness':14,
-     'Civil Engineer':5, 'Java Developer':15, 'Business Analyst':4,
-    'SAP Developer':21, 'Automation Testing':2, 'Electrical Engineering':11,
-    'Operations Manager':18, 'Python Developer':20, 'DevOps Engineer':8,
-    'Network Security Engineer':17, 'PMO':19, 'Database':7, 'Hadoop':13,
-    'ETL Developer':10, 
-    'DotNet Developer':9, 'Blockchain':3, 'Testing':23}
-category_name=category_mapping.get(prediction_id,'unknown')
-st.write('predicted category:',category_name)
+        category_mapping={'Data Science':6, 'HR':12, 'Advocate':0, 'Arts':1, 'Web Designing':24,
+        'Mechanical Engineer':16, 'Sales':22, 'Health and fitness':14,
+        'Civil Engineer':5, 'Java Developer':15, 'Business Analyst':4,
+         'SAP Developer':21, 'Automation Testing':2, 'Electrical Engineering':11,
+         'Operations Manager':18, 'Python Developer':20, 'DevOps Engineer':8,
+        'Network Security Engineer':17, 'PMO':19, 'Database':7, 'Hadoop':13,
+        'ETL Developer':10, 
+         'DotNet Developer':9, 'Blockchain':3, 'Testing':23,
+                         }
+        category_name=category_mapping.get(prediction_id,'unknown')
+        st.write('predicted category:',category_name)
  
  
 if __name__="__main__":
